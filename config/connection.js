3c37e44ded3e47d845895ca9c7bb81f2616bfc5c
katcondev <kat.contreras@gmail.com>
2024-01-26 08:49:01 -0800

    updated to rds

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,6 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
 const fs = require("fs");
-const rdsCa = fs.readFileSync(__dirname + "/rds-combined-ca-bundle.pem");
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
@@ -11,12 +10,6 @@ const sequelize = new Sequelize(
     host: "wordgamedb.ca2rma9t9xos.us-west-1.rds.amazonaws.com",
     dialect: "postgres",
     port: 5432,
-    dialectOptions: {
-      ssl: {
-        rejectUnauthorized: true,
-        ca: [rdsCa],
-      },
-    },
   }
 );
 
